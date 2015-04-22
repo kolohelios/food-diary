7c14fcd8a680d984feb5ff3299e92142a5ef8624
Jon Edwards <jkedwards@me.com>
2015-04-22 15:05:17 -0700

    add table to display foods and ending weight calculation

--- client/index.js
+++ client/index.js
@@ -5,28 +5,57 @@ angular.module('food-diary', ['firebase'])
   $rootScope.fbRoot = new $window.Firebase('https://fooddiary-kolohelios.firebaseio.com/');
 
 }])
-.controller('master', ['$scope', '$firebaseObject', function($scope, $firebaseObject){
+.controller('master', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray){
 
   var fbUser = $scope.fbRoot.child('user');
   var afUser = $firebaseObject(fbUser);
 
   $scope.user = afUser;
 
+  $scope.showUserForm = function(){
+    $scope.isUserFormShown = true;
+  };
+
   $scope.saveUser = function(){
     $scope.user.$save();
-    calcBMI();
+    calcBMI($scope.user.weight);
+    $scope.isUserFormShown = false;
   };
 
   $scope.user.$loaded().then(function(){
-    calcBMI();
+    calcBMI($scope.user.weight);
   });
 
-  function calcBMI(){
+  function calcBMI(weight){
     if($scope.user.heightunits === 'inches'){
-      $scope.bmi = ($scope.user.weight * 703) / Math.pow($scope.user.height, 2);
+      $scope.bmi = (weight * 703) / Math.pow($scope.user.height, 2);
     }
     else{
-      $scope.bmi = ($scope.user.weight) / Math.pow($scope.user.height / 100, 2);
+      $scope.bmi = (weight) / Math.pow($scope.user.height / 100, 2);
     }
+  }
+
+  var fbFoods = $scope.fbRoot.child('foods');
+  var afFoods = $firebaseArray(fbFoods);
+
+  $scope.foods = afFoods;
+
+  $scope.foods.$loaded().then(function(){
+     calcWeight();
+  });
+
+  $scope.addFood = function(){
+    $scope.foods.$add($scope.food);
+    var now = new Date();
+    $scope.food.date = now.getTime();
+    calcWeight();
+  };
+
+  function calcWeight(){
+    var totalCals = 0;
+    $scope.foods.forEach(function(f){
+      totalCals += f.calsPerServing * f.servings;
+    });
+    $scope.endWeight = $scope.user.weight + totalCals / 3500;
   }
 }]);
